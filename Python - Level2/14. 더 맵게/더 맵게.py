eddf5cb20e46a81929affc27880617ce63675342
hwajeong <honeylee999@icloud.com>
2021-04-11 21:10:02 +0900

    Algorithm self study

--- Python - Level2/14. 더 맵게/더 맵게.py
+++ Python - Level2/14. 더 맵게/더 맵게.py
@@ -1,5 +1,23 @@
+import heapq
+
 scoville = [1, 2, 3, 9, 10, 12]
 K = 7
 
 def solution(scoville, K):
-    pass
+    # 모든 음식의 스코빌 지수가 k 이상 => min이 k 이상
+    # 아니면 min(최소) remove하고 min(두번째)
+
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if len(scoville) <= 1:
+            return -1
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
+        result = min1 + min2 * 2
+        heapq.heappush(scoville, result)
+        answer += 1
+
+    return answer
+
+print(solution(scoville, K))
--- Python - Level2/46. 뉴스 클러스터링/뉴스 클러스터링.py
+++ Python - Level2/46. 뉴스 클러스터링/뉴스 클러스터링.py
@@ -1,49 +1,61 @@
-str1 = 'aa1+aa2'
+str1 = 'aaaa'
 str2 = 'AAAA12'
 
 # str1 = aa aa
 # str2 = aa aa aa
 
+print(str1.isalpha())
+
+# 1. 문자열을 원하는 형태(다중집합)로 치환해주는 함수
 def change_string(str):
     str_set = []
+    # 문자열을 소문자 리스트로 만들어주고
     str = list(str.lower())
 
+    # 전체 길이의 -1까지 두개씩 잘라서 준비해둔 str_set리스트에 넣어줌
     for i in range(0, len(str)-1):
         if str[i].isalpha() and str[i+1].isalpha():
             str_set.append(''.join([str[i], str[i+1]]))
 
     return str_set
 
-
+# 2. 자카드 유사도를 검사하는 함수
 def solution(str1, str2):
+    # 두개의 문자열을 각각 두개씩 잘라서 다중집합으로 만들고
     str1_set = change_string(str1)
     str2_set = change_string(str2)
     # 결과
     result = 0
 
-    #교집합
+    # 2-1. 교집합 구하기
+    #우선 두 다중함수 리스트에서 교집합을 구하고
     intersection = set(str1_set) & set(str2_set)
-    intersection_final = []
+    #찐교집합의 갯수가 들어갈 리스트에
+    intersection_final = []  #2
     for i in intersection:
+        #두 개의 리스트에서 교집합요소의 갯수를 세서 작은 걸 넣고
         intersection_final.append(min(str1_set.count(i), str2_set.count(i)))
 
-    #합집합
+    # 2-2. 합집합 구하기
     union = set(str1_set) | set(str2_set)
-
     union_final = []
+    # 위와 마찬가지로
     for i in union:
-        union_final.append(max(str1_set.count(i), str2_set.count(i)))
-
+        # 이번에는 합집합 요소의 갯수를 세서 큰걸 넣음
+        union_final.append(max(str1_set.count(i), str2_set.count(i))) #3
 
     try:
+        # 자카드 유사도는 교집합 / 합집합
         result = sum(intersection_final) / sum(union_final)
     except:
+        # zero divison 시 그냥 65536
         return 65536
 
+    # 교집합이 없으면 0
     if result == 0:
         return 0
+    # 이외에는 정수형태 * 655536
     return int(result * 65536)
-
 
 
 print(solution(str1, str2))
