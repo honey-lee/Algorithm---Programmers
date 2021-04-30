736986faabf08ba9cd2eb82f3f4d3376a2e238a3
hwajeong <honeylee999@icloud.com>
2021-04-30 11:09:45 +0900

    2021-04-30 APS

---
+++ Python - Level2/주식 가격/s2.py
@@ -0,0 +1,17 @@
+prices = [1, 2, 3, 2, 3, 1]
+k = len(prices)
+stack = []
+
+def push(item):
+    pass
+
+
+def solution(prices):
+    pass
+
+
+
+
+
+
+print(solution(prices))
--- Python - Level2/주식 가격/주식 가격.py
+++ Python - Level2/주식 가격/주식 가격.py
@@ -1,7 +1,27 @@
-prices = [1, 2, 3, 2, 3]
+prices = [1, 2, 3, 2, 3, 1]
+
+"""
+효율성 실패
+"""
 
 def solution(prices):
-    answer = [0] * len(prices)
+    answer = [i for i in range(len(prices)-1, -1, -1)]
+
+    for i in range(len(prices)):
+        if prices[i] == min(prices[i:]):
+            continue
+        else:
+            n_prices = prices[i:]
+            for j in range(len(n_prices)):
+                if n_prices[j] < prices[i]:
+                    answer[i] = j
+                    break
+
+    return answer
+
+print(solution(prices))
+
+
 
 
 
