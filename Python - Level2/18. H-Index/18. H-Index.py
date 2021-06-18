d36edebf45f960182d5da1b4bfa36c6fe9026165
hwajeong <honeylee999@icloud.com>
2021-06-18 23:56:14 +0900

    2021-06-18 APS

--- Python - Level2/18. H-Index/18. H-Index.py
+++ Python - Level2/18. H-Index/18. H-Index.py
@@ -1,27 +1,22 @@
+"""
+H-index : 특정 연구원의 연구 성과를 평가하기 위한 지표, 발표한 논문수와 피인용수를 이용해 측정
+
+논문중 피인용수가 많은 순서로 정렬해, 피인용수가 논문수와 같아지거나 피인용수가 논문수보다 작아지기 시작하는 숫자가
+나의 H-index이다.
+
+"""
+
 citations = [3, 0, 6, 1, 5]
 
 def solution(citations):
-    # 전체 작성된 논문의 갯수
-    n = len(citations)
-    # H index 지수, 최대값이 전체 논문의 수와 같다
-    h = len(citations)
-    # 인용된 횟수를 오름차순으로 정렬
-    citations.sort()
-    cnt_h = 0
+    ans = 0
+    max_h = max(citations)
 
-    if h != 0:
-        for citation in citations:
-            if citation >= h:
-                cnt_h +=1
-            else:
-                continue
+    for h in range(max_h, 0, -1):
+        if len([i for i in citations if i >= h]) >= h:
+            return h
 
-            if cnt_h == h:
-                break
-            else:
-                h -= 1
-
-    return h
+    return ans
 
 
 
