71df3f45983d7bb8cac627a044c5e122369fb630
hwajeong <honeylee999@icloud.com>
2021-06-26 23:23:22 +0900

    2021-06-26 APS

--- Python - Level2/수식 최대화/수식 최대화.py
+++ Python - Level2/수식 최대화/수식 최대화.py
@@ -2,25 +2,45 @@ from itertools import permutations
 import copy
 
 def solution(expression):
-    ans = 0
-    order = list(permutations(['+', '-', '*'], 3))
-    stack = []
-    tmp = ''
-    expression1 = copy.deepcopy(expression)
-
+    ans = []
+    # 연산자 적용 순서
+    order = list(permutations(['+', '-', '*']))
+    # 계산 대상 숫자
+    nums = list(map(int, expression.replace('+', ' ').replace('-', ' ').replace('*', ' ').split(' ')))
+    # 연산자들 (operator)
+    op = []
     for i in range(len(expression)):
-        if expression[i].isnumeric():
-            tmp += expression[i]
-        else:
-            stack.append(int(tmp))
-            stack.append(str(expression[i]))
-            tmp = ''
-    stack.append(tmp)
+        if not expression[i].isnumeric():
+            op.append(expression[i])
 
-    expression1 = expression1.split('*')
+    while order:
+        ans.append(calc(op, nums, order.pop(0)))
 
+    return max(ans)
 
-    return expression1
+def calc(op, nums, op_):
+    nums2 = copy.deepcopy(nums)
+    op2 = copy.deepcopy(op)
+
+    for i in op_:
+        while i in op2:
+            idx = op2.index(i)
+            if i == '+':
+                nums2[idx] = nums2[idx] + nums2[idx+1]
+                nums2.pop(idx+1)
+                op2.pop(idx)
+            elif i == '-':
+                nums2[idx] = nums2[idx] - nums2[idx+1]
+                nums2.pop(idx + 1)
+                op2.pop(idx)
+            elif i == '*':
+                nums2[idx] = nums2[idx] * nums2[idx+1]
+                nums2.pop(idx + 1)
+                op2.pop(idx)
+
+    if nums2[0] >= 0:
+        return nums2[0]
+    return -nums2[0]
 
 
 
