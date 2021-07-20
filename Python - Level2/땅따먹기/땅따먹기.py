4376377569bbd5db63848de90968d53a2aedaee5
hwajeong <honeylee999@icloud.com>
2021-07-20 20:51:05 +0900

    2021-07-20 APS DP

---
+++ Python - Level2/땅따먹기/땅따먹기.py
@@ -0,0 +1,20 @@
+"""
+Dynamic Programming
+
+"""
+
+def solution(land):
+    n = len(land) - 1
+
+    for i in range(n):
+        land[i + 1][0] += max(land[i][1], land[i][2], land[i][3])
+        land[i + 1][1] += max(land[i][0], land[i][2], land[i][3])
+        land[i + 1][2] += max(land[i][0], land[i][1], land[i][3])
+        land[i + 1][3] += max(land[i][0], land[i][1], land[i][2])
+
+    return max(land[n][0], land[n][1], land[n][2], land[n][3])
+
+
+land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]]
+
+print(solution(land))
