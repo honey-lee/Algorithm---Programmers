f6bd9e0b84576b628b8457dad1fb1d9a7380fca3
honey-lee <honeylee999@icloud.com>
2021-11-28 23:38:56 +0900

    2021-11-28 APS

--- Python - Level2/n^2 배열 자르기/n^2 배열 자르기.py
+++ Python - Level2/n^2 배열 자르기/n^2 배열 자르기.py
@@ -1,12 +1,7 @@
 def solution(n, left, right):
     answer = []
-    cnt = 1
-    for i in range(n):
-        for j in range(1, n + 1):
-            if cnt <= j:
-                answer.append(j)
-            else:
-                answer.append(cnt)
-        cnt += 1
 
-    return answer[left:right + 1]
+    for i in range(left, right + 1):
+        answer.append(max(divmod(i, n)) + 1)
+
+    return answer
